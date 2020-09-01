83caed56025df192bb7c9061135ae36247941248
Toluwalope Akinkunmi <64863887+dynamix23@users.noreply.github.com>
2020-09-01 10:54:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+//my javascript animation code
+document.querySelector('li').innerHTML = '<h3>Hello</h3>'
+console.log()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Animations
+hover
+on mouse click
+slide in 
+footer
