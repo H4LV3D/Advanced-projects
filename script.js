399bc84c2d7d88be9fec4b052a6a2890f7b40518
dynamix23 <akinkunmitolulope23@gmail.com>
2020-09-08 10:38:17 +0100

    moved scripts

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 // WELCOME TO JAVASCRIPT
 
-console.dir(document);
-console.log(document.domain);
-console.log(document.URL);
-console.dir(window);
-console.log(document.doctype);
-console.log(document.head);
-console.log(document.all);
-
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.dir(window);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.getElementsByClassName('table1'));
+// console.log(document.all[10]);
+// console.log(document)
+// var items = document.querySelector('#items');
+// console.log(items);
+// console.log(items.parentNode);
+// items.parentNode.style.backgroundColor = '#f4f4f4';
+var button = document.getElementById('btn').addEventListener
+('click', function(){
+    console.log(123);
+});
